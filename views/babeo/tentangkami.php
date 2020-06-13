3622b9d24947df6e2eb3baa643e5df53c0844550
17Bagusfachruddin <57518845+17Bagusfachruddin@users.noreply.github.com>
2020-06-13 18:49:13 +0700

    Create tentangkami.php

---
+++ views/babeo/tentangkami.php
@@ -0,0 +1,16 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-7 py-5 wrap-about pb-md-5 ftco-animate">
+			<div class="heading-section-bold mb-5 mt-md-5">
+				<div class="ml-md-0">
+					<h1 class="mb-4">BABEO </h1>
+					<h3>(Barang Bekas Online) </h3>
+				</div>
+			</div>
+			<div class=" pb-md-5">
+				<p>BABEO merupakan aplikasi jual beli secara online yang fokus pada penjualan barang bekas . Barang bekas yang dijual pada aplikasi yaitu berbagai macam fashion . Dimulai dari fashion atasan dan bawahan wanita, fashion atasan dan bawahan pria, serta aksesoris. aksesoris pun yang dijual bermacam-macam. Aksesoris yang dijual yaitu mulai dari jam tangan, kalung , anting-anting, jilbab, sepatu, sendal, tas, ikat pinggang, dll. </p>
+				<p>Aplikasi BABEO ini dapat diakses oleh seluruh masyarakat. semua pengguna dapat menjadi pembeli atau penjual pada aplikasi ini. tujuan dibuatnya aplikasi adalah untuk memudahkan orang-orang yang ingin menjual pakaian atau aksesorisnya dengan lebih cepat dan mudah serta terjamin kualitas dan keamanannya . </p>
+			</div>
+		</div>
+	</div>
+</div>
